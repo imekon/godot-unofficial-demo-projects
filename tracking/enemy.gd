062016cd7ecae1f420f5469dcee5b11af42a8d19
Pete Goodwin <github@imekon.org>
2017-07-30 11:35:38 +0100

    random movement on collision

--- tracking/enemy.gd
+++ tracking/enemy.gd
@@ -6,14 +6,20 @@ var velocity = Vector2()
 
 func _ready():
 	set_fixed_process(true)
-	velocity = Vector2(0.1 * WALK_SPEED, 0.9 * WALK_SPEED)
+	compute_direction()
 
 func _fixed_process(delta):
 	var motion = velocity * delta
 	motion = move(motion)
 	
 	if (is_colliding()):
-		var n = get_collision_normal()
-		motion = n.slide(motion)
-		velocity = n.slide(velocity)
-		move(motion)
+		#var n = get_collision_normal()
+		#motion = n.slide(motion)
+		#velocity = n.slide(velocity)
+		#move(motion)
+		compute_direction()
+
+func compute_direction():
+	var x = 0.5 - randf()
+	var y = 0.5 - randf()
+	velocity = Vector2(x * WALK_SPEED, y * WALK_SPEED)
