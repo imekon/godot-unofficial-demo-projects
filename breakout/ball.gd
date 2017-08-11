5f64164cb18f2946431a28356651bd4d87849529
Pete Goodwin <pgoodwin@imekon.org>
2017-08-11 07:14:37 +0100

    get ball and bat going

--- breakout/ball.gd
+++ breakout/ball.gd
@@ -1,9 +1,21 @@
 extends RigidBody2D
 
-var v
+var ball
 
 func _ready():
 	var n = randf() * PI / 2
-	v = Vector2(sin(n), -cos(n))
-	var ball = get_node(".")
-	ball.set_linear_velocity(v * 200)
+	ball = get_node(".")
+	compute_velocity()
+	set_fixed_process(true)
+
+func compute_velocity():
+	var n = randf() * PI / 2
+	var velocity = Vector2(sin(n), -cos(n))
+	ball.set_linear_velocity(velocity * 200)
+		
+func _fixed_process(delta):
+	var pos = ball.get_pos()
+	if pos.y > 650:
+		ball.set_pos(Vector2(512, 400))
+		compute_velocity()
+
--- breakout/bat.gd
+++ breakout/bat.gd
@@ -1,4 +1,19 @@
 extends KinematicBody2D
 
+const WALK_SPEED = 400
+
+var velocity = Vector2()
+
 func _ready():
-	pass
+	set_fixed_process(true)
+	
+func _fixed_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = WALK_SPEED
+	else:
+		velocity.x = 0
+		
+	var motion = velocity * delta
+	move(motion)
