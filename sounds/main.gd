0faa1aaff065c47464283b2a89a25adadc6b076c
Pete Goodwin <pgoodwin@imekon.org>
2017-11-24 08:19:04 +0000

    trying to get tween to fade out sounds

--- sounds/main.gd
+++ sounds/main.gd
@@ -1,21 +1,29 @@
 extends PanelContainer
 
 onready var samplePlayer = get_node("SamplePlayer")
+onready var thrustTween = get_node("ThrustTween")
 
 var engine
 var blackHole
 
 func _ready():
-	pass
+	thrustTween.interpolate_method(self, "setEnginedB", 0.0, -60.0, 5.0, Tween.TRANS_QUAD, Tween.EASE_OUT)
 
 func _onThrust():
 	engine = samplePlayer.play("engine")
+	thrustTween.reset(self, "setEnginedB")
 
 func _onBlackHole():
 	blackHole = samplePlayer.play("artic drone")
 
 func _onStopThrust():
-	samplePlayer.stop(engine)
+	thrustTween.start()
 
 func _onStopBlackHole():
 	samplePlayer.stop(blackHole)
+
+func setEnginedB(db):
+	samplePlayer.set_volume_db(db)
+
+func _onThrustTweenComplete( object, key ):
+	samplePlayer.stop(engine)
