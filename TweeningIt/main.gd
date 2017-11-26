5cbb725cef856686a895bd0057e26036bd0d13f7
imekon <github@imekon.org>
2017-11-26 17:52:11 +0000

    move interpolate to actual operation

--- TweeningIt/main.gd
+++ TweeningIt/main.gd
@@ -5,13 +5,12 @@ onready var tweenIn = get_node("TweenIn")
 onready var label = get_node("Panel/Label")
 
 func _ready():
-	tweenOut.interpolate_property(label, "visibility/opacity", 1.0, 0.0, 3.0, Tween.TRANS_QUAD, Tween.EASE_OUT)
-	tweenIn.interpolate_property(label, "visibility/opacity", 0.0, 1.0, 3.0, Tween.TRANS_QUAD, Tween.EASE_IN)
+	pass
 
 func _onFadeOut():
-	tweenOut.reset(label, "visibility/opacity")
+	tweenOut.interpolate_property(label, "visibility/opacity", 1.0, 0.0, 3.0, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	tweenOut.start()
 
 func _onFadeIn():
-	tweenIn.reset(label, "visibility/opacity")
+	tweenIn.interpolate_property(label, "visibility/opacity", 0.0, 1.0, 3.0, Tween.TRANS_QUAD, Tween.EASE_IN)
 	tweenIn.start()
