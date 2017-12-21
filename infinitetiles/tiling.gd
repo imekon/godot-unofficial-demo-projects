954a390c49e1d5bd7d71b88aadf89a6b1b426491
Pete Goodwin <pgoodwin@imekon.org>
2017-12-21 16:08:35 +0000

    added snow shader

--- infinitetiles/tiling.gd
+++ infinitetiles/tiling.gd
@@ -6,10 +6,14 @@ var x = 0
 var y = 0
 var w = 0
 var h = 0
+var w2 = 0
+var h2 = 0
 
 func _ready():
 	w = seamless.get_width()
 	h = seamless.get_height()
+	w2 = w / 2
+	h2 = h / 2
 	set_process(true)
 	
 func _process(delta):
@@ -29,5 +33,5 @@ func _draw():
 	draw_texture(seamless, Vector2(x - w, y + h))
 	
 func setLocation(playerX, playerY):
-	x = int(playerX / w) * w
-	y = int(playerY / h) * h
+	x = int((playerX - w2) / w) * w
+	y = int((playerY - h2) / h) * h
